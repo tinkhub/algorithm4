6ec929bc0680cc97b6dd758ad83f9780bda36147
Ken Hoegun You <saykent@gmail.com>
2019-04-12 00:06:20 +1000

    Add Stack

---
+++ src/main/java/com/tinkhub/algs4/fundamental/StackClient.java
@@ -0,0 +1,18 @@
+package com.tinkhub.algs4.fundamental;
+
+import com.tinkhub.algs4.utils.StdIn;
+import com.tinkhub.algs4.utils.StdOut;
+
+public class StackClient {
+    public static void main(String[] args)
+    {
+        Stack<String> s = new Stack<>();
+
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-")) s.push(item);
+            else if (!s.isEmpty()) StdOut.print(s.pop() + " ");
+        }
+        StdOut.println("(" + s.size() + " left on stack)");
+    }
+}
